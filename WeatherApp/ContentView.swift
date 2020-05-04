9f826d9a715dc2238d0ca820f7e08889360e8f49
Nancy El-Sharkawi <elsharkawinancy@gmail.com>
2020-05-04 17:15:33 +0300

    Initial Commit

---
+++ WeatherApp/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  WeatherApp
+//
+//  Created by Nour El-Sharkawi on 5/4/20.
+//  Copyright © 2020 Nancy El-Sharkawi. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
